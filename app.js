a9bc9234bcd0668093841e09c2bc96543dadffb7
tomiyu <tomitayu1212@gmail.com>
2021-11-17 00:57:57 +0900

    master

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+app.use(express.static("public"));
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+    res.render('index.ejs');
+});
 
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
