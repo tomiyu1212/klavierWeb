d8d246f5aaee09a5694c35e760273268c80bba0d
tomiyu <tomitayu1212@gmail.com>
2022-02-09 00:26:39 +0900

    main

--- server/controllers/urlController.js
+++ server/controllers/urlController.js
@@ -2,10 +2,10 @@ const mysql = require ('mysql');
 
 // Connection Pool
 const pool = mysql.createPool ({
-    host     : process.env.DB_HOST,
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASS,
-    database : process.env.DB_NAME
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 exports.view = (req, res) => {
