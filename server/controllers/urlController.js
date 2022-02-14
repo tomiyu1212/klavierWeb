a50c11b36ca50164efa4d35817499b484ce4edaf
tomiyu <tomitayu1212@gmail.com>
2022-02-15 00:16:02 +0900

    main

--- server/controllers/urlController.js
+++ server/controllers/urlController.js
@@ -3,6 +3,7 @@ const mysql = require ('mysql');
 // Connection Pool
 const pool = mysql.createPool ({
     host: process.env.DB_HOST,
+    port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
